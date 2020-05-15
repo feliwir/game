cb173862980cf907b0f2294bfe162abb43fae54d
Stephan Vedder <stephan.vedder@gmail.com>
2020-05-15 22:45:58 +0200

    Migrate to unity

---
+++ assets/Scripts/Chunk.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+
+    int vertexIndex = 0;
+    List<Vector3> vertices = new List<Vector3>();
+    List<int> triangles = new List<int>();
+    List<Vector2> uvs = new List<Vector2>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int y = 0; y<VoxelData.ChunkHeight;y++)
+        {
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
+            {
+                for (int z = 0; z < VoxelData.ChunkWidth; z++)
+                {
+                    AddVoxelDataToChunk(new Vector3(x,y,z));
+                }
+            }
+        }
+        CreateMesh();
+    }
+
+    void CreateMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.RecalculateNormals();
+
+        meshFilter.mesh = mesh;
+    }
+
+    void AddVoxelDataToChunk(Vector3 pos)
+    {
+        for (int p = 0; p < 6; p++)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                int triangleIndex = VoxelData.voxelTris[p, i];
+                vertices.Add(VoxelData.voxelVerts[triangleIndex] + pos);
+                triangles.Add(vertexIndex);
+                uvs.Add(VoxelData.voxelUvs[i]);
+                vertexIndex++;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ assets/Scripts/VoxelData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VoxelData
+{
+    public static int ChunkWidth = 5;
+    public static int ChunkHeight = 5;
+
+    public static readonly Vector3[] voxelVerts = new Vector3[8] 
+    {
+        new Vector3(0.0f,0.0f,0.0f),
+        new Vector3(1.0f,0.0f,0.0f),
+        new Vector3(1.0f,1.0f,0.0f),
+        new Vector3(0.0f,1.0f,0.0f),
+        new Vector3(0.0f,0.0f,1.0f),
+        new Vector3(1.0f,0.0f,1.0f),
+        new Vector3(1.0f,1.0f,1.0f),
+        new Vector3(0.0f,1.0f,1.0f),
+    };
+
+    public static readonly int[,] voxelTris = new int[6, 6]
+    {
+        { 0,3,1,1,3,2 }, // BACK FACE
+        { 5,6,4,4,6,7 }, // FRONT FACE
+        { 3,7,2,2,7,6 }, // TOP FACE
+        { 1,5,0,0,5,4 }, // BOTTOM FACE
+        { 4,7,0,0,7,3 }, // LEFT FACE
+        { 1,2,5,5,2,6 }  // RIGHT FACE
+    };
+
+    public static readonly Vector2[] voxelUvs = new Vector2[6]
+    {
+        new Vector2(0.0f, 0.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(1.0f, 0.0f),
+        new Vector2(1.0f, 0.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(1.0f, 1.0f)
+    };
+}
